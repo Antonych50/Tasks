9bea430dcf8cd300ffa58ad3c03ab186e31f8fd7
Antonych50 <Antonych50@gmail.com>
2024-12-16 10:47:47 +0300

    Completed

--- main.py
+++ main.py
@@ -1,22 +1,33 @@
-from datetime import datetime
+from datetime import datetime, timedelta
 
 
 class TasksControl:
     tasks = []
     @staticmethod
-    def add_task(obj):
-        return TasksControl.tasks.append(obj)
+    def add_task(task):
+        return TasksControl.tasks.append(task)
+
+    @staticmethod
+    def delete_task(task):
+        TasksControl.tasks.remove(task)
 
     @staticmethod
     def print_uncompleted():
-        for i in range(len(list(TasksControl.tasks))):
-            if (TasksControl.tasks[i].status == "No") :
-                print(f"{TasksControl.tasks[i].description}")
-
-
+        #for i in range(len(list(TasksControl.tasks))):
+            #if (TasksControl.tasks[i].status == "No") :
+                #print(f"{TasksControl.tasks[i].description}")
+        for tsk in TasksControl.tasks:
+            if tsk.status_get() == "No":
+                print(f"{tsk.description_get()} не выполнена, срок выполнения: {tsk.release_date_get()}")
+            else:
+                print(f"Задача {tsk.description_get()} выполнена, удалить ее из списка?(Y|N)")
+                tmp = input()
+                if tmp == "Y":
+                    TasksControl.delete_task(tsk)
 
 
 class Task():
+
     def __init__(self, description="", release_date=datetime.now().date(), status="No"):
         self.description = description
         self.release_date = release_date
@@ -40,11 +51,23 @@ class Task():
     def status_get(self):
         return self.status
 
+    def __str__(self):
+        return f'{self.description}; {self.release_date}; {self.status}'
 
-t = Task()
+
+t = Task("Problem1")
+t1 = Task("Problem2","01/01/2025")
 #t.status_set("")
-print(f"{t.release_date} {t.status}")
-TasksControl.tasks= TasksControl.add_task(t)
-#TasksControl.print_uncompleted()
+print(f"{t.release_date_get()} {t.status_get()}")
 print(TasksControl.tasks)
+#tctrl = TasksControl()
+#tctrl.add_task(t)
+#tctrl.add_task(t1)
+TasksControl.add_task(t)
+TasksControl.add_task(t1)
+#TasksControl.print_uncompleted()
+print(TasksControl.tasks[1].status_get())
+TasksControl.tasks[1].status_set("Yes")
+print(TasksControl.tasks[1].status)
+TasksControl.print_uncompleted()
 
